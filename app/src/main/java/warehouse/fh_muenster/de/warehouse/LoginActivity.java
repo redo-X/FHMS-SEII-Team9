c48f612370eb8a454c5ff1648d81c59d5b1fc111
FutureDragon <future_dragon@hotmail.de>
2016-05-09 15:42:00 +0200

    Add ClickListener Login_btn in LoginActivity

--- app/src/main/java/warehouse/fh_muenster/de/warehouse/LoginActivity.java
+++ app/src/main/java/warehouse/fh_muenster/de/warehouse/LoginActivity.java
@@ -2,6 +2,10 @@ package warehouse.fh_muenster.de.warehouse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final Button login_btn = (Button) findViewById(R.id.login_btn);
+        login_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                EditText password_text = (EditText) findViewById(R.id.password_txt);
+                EditText username_txt = (EditText) findViewById(R.id.name_txt);
+
+
+
+            }
+
+        });
+
     }
 }
