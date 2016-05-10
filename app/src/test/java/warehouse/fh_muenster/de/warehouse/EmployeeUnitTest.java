845cb2d529b96de930b93be4b8e8e95bd5739b9d
FutureDragon <future_dragon@hotmail.de>
2016-05-10 11:50:25 +0200

    Added EmployeeTestUnit

---
+++ app/src/test/java/warehouse/fh_muenster/de/warehouse/EmployeeUnitTest.java
@@ -0,0 +1,15 @@
+package warehouse.fh_muenster.de.warehouse;
+
+import org.junit.Test;
+
+/**
+ * Created by Thomas on 10.05.2016.
+ */
+public class EmployeeUnitTest {
+
+    @Test
+    public void login_isCorrect(){
+        Employee user = new Employee("Bob","123");
+
+    }
+}
