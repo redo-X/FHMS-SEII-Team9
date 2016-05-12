f26d4f051c8a433f0105e30801593be0ca891f62
FutureDragon <future_dragon@hotmail.de>
2016-05-12 16:25:26 +0200

    Change LoginScreen Layout

--- app/src/main/java/warehouse/fh_muenster/de/warehouse/LoginActivity.java
+++ app/src/main/java/warehouse/fh_muenster/de/warehouse/LoginActivity.java
@@ -92,6 +92,7 @@ public class LoginActivity extends AppCompatActivity {
                 //Nächste Activity aufrufen
                 Intent i = new Intent(context, CommissioningOverview.class);
                 startActivity(i);
+                // Beim Klick auf den Back Button wird der Login Screen nicht mehr aufgerufen sondern die app beendet
                 finish();
             }
 
