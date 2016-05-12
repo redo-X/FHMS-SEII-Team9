e2f5e3a5140e40b70a1e8bb42de0309516c025d2
FutureDragon <future_dragon@hotmail.de>
2016-05-12 14:45:05 +0200

    Add Table to CommissioningOverview

--- app/src/main/java/warehouse/fh_muenster/de/warehouse/CommissioningOverview.java
+++ app/src/main/java/warehouse/fh_muenster/de/warehouse/CommissioningOverview.java
@@ -2,6 +2,10 @@ package warehouse.fh_muenster.de.warehouse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class CommissioningOverview extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class CommissioningOverview extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_commissioning_overview);
+
+        TableLayout table = (TableLayout) findViewById(R.id.table_layout);
+
+        for(int i = 0; i<=5;i++){
+            TableRow row = new TableRow(this);
+            TextView spalte1 = new TextView(this);
+            TextView spalte2 = new TextView(this);
+            TextView spalte3 = new TextView(this);
+
+            spalte1.setText("Zeile " + i);
+            spalte2.setText("Zeile " + i);
+            spalte3.setText("Zeile " + i);
+
+            row.addView(spalte1);
+            row.addView(spalte2);
+            row.addView(spalte3);
+            table.addView(row);
+        }
+
+
     }
+
+
+
+
+
 }
