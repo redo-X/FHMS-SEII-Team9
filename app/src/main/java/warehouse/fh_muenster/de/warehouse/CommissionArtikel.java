12c457cd0d78442f812a2c415b9eee950504d5ff
FutureDragon <future_dragon@hotmail.de>
2016-05-13 18:28:13 +0200

    Add TableLayout to CommissionArtikel

--- app/src/main/java/warehouse/fh_muenster/de/warehouse/CommissionArtikel.java
+++ app/src/main/java/warehouse/fh_muenster/de/warehouse/CommissionArtikel.java
@@ -1,10 +1,22 @@
 package warehouse.fh_muenster.de.warehouse;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.util.Random;
+
 public class CommissionArtikel extends AppCompatActivity {
+    int artikelZaehler = 1;
+    int artikelGesamt = 8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,15 +24,58 @@ public class CommissionArtikel extends AppCompatActivity {
         setContentView(R.layout.activity_commission_artikel);
 
         int id = getIntent().getExtras().getInt("id");
+        final Button weiter_btn = (Button) findViewById(R.id.weiter_btn);
+
         TextView ueberschrift = (TextView) findViewById(R.id.commission_id_label);
         TextView artikelanzahlLabel = (TextView) findViewById(R.id.commission_artikelAnzahl_label);
 
-        int artikelZaehler = 1;
-        int artikelGesamt = 8;
+
+
+
+
 
         ueberschrift.setText("Kommission mit der Nummer: " + String.valueOf(id) + " ausgewählt\n");
         artikelanzahlLabel.setText("Artikel " + artikelZaehler +  " von " + artikelGesamt);
 
 
+        weiter_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if(artikelZaehler >= artikelGesamt){
+                    finish();
+                }
+                else {
+
+                    TextView artikelanzahlLabel = (TextView) findViewById(R.id.commission_artikelAnzahl_label);
+                    artikelZaehler++;
+                    artikelanzahlLabel.setText("Artikel " + artikelZaehler + " von " + artikelGesamt);
+
+                    TextView a = (TextView) findViewById(R.id.commission_artikel_artikel_name);
+                    a.setText("asdfsdklasd");
+
+                    if(artikelZaehler >= artikelGesamt){
+                        weiter_btn.setText("Kommession beenden");
+                    }
+                }
+            }
+        });
+
     }
+
+    private void printTable(){
+        TableLayout table = (TableLayout) findViewById(R.id.table_layout);
+        TableRow row = new TableRow(this);
+
+
+        // Erzeugen der Spalten
+
+
+            //Hinzufügen der Spalten
+
+
+
+            table.addView(row);
+
+    }
+
+
 }
