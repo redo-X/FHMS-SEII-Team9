06c3ef1a315dd4d4423e0c1767d5d84a0be4e564
FutureDragon <future_dragon@hotmail.de>
2016-05-14 15:49:18 +0200

    Change weiter Button logic CommissionArticel, Add BackPressed Action

--- app/src/main/java/warehouse/fh_muenster/de/warehouse/CommissionArtikel.java
+++ app/src/main/java/warehouse/fh_muenster/de/warehouse/CommissionArtikel.java
@@ -2,6 +2,7 @@ package warehouse.fh_muenster.de.warehouse;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -31,58 +32,38 @@ public class CommissionArtikel extends AppCompatActivity {
         TextView ueberschrift = (TextView) findViewById(R.id.commission_id_label);
         TextView artikelanzahlLabel = (TextView) findViewById(R.id.commission_artikelAnzahl_label);
 
-
-
-
-
-
         ueberschrift.setText("Kommission mit der Nummer: " + String.valueOf(id) + " ausgewählt\n");
         artikelanzahlLabel.setText("Artikel " + artikelZaehler +  " von " + artikelGesamt);
 
 
         weiter_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                // Letzer Artikel, beenden der Activity und anzeigen von erfollgs meldung
                 if(artikelZaehler >= artikelGesamt){
+                    showToast("Kommission erfolgreich abgeschlossen");
                     finish();
                 }
                 else {
-                    // Setzen der neuen Überschrift
-                    TextView artikelanzahlLabel = (TextView) findViewById(R.id.commission_artikelAnzahl_label);
-                    artikelanzahlLabel.setText("Artikel " + artikelZaehler + " von " + artikelGesamt);
-                    // Setzen der ganzen Zeilen
-                    TextView artikelCode = (TextView) findViewById(R.id.commission_artikel_artikel_code);
-                    artikelCode.setText("456");
-
-                    TextView artikelName = (TextView) findViewById(R.id.commission_artikel_artikel_name);
-                    artikelName.setText("asdfsdklasd");
-
-                    TextView lagerort = (TextView) findViewById(R.id.commission_artikel_artikel_storage);
-                    lagerort.setText("5");
-
-                    TextView lagerbestand = (TextView) findViewById(R.id.commission_artikel_artikel_soll);
-                    lagerbestand.setText("30");
-
-                    TextView kommissionsMenge = (TextView) findViewById(R.id.commission_artikel_artikel_commession);
-                    kommissionsMenge.setText("5");
-
+                    // Noch mehere Artikel zu Kommissionieren
+                    // Einlesen der zu Kommissionierten Menge
                     EditText kommissionierteMenge_txt = (EditText) findViewById(R.id.commission_artikel_artikel_commession_edit);
                     int kommissionierteMenge = 0;
                     try{
                         //Wenn gültige Menge eingegben kann nächster Artikel aufgerufen werden
                         kommissionierteMenge = Integer.valueOf(kommissionierteMenge_txt.getText().toString());
-                        //@TODO Prüfen ob kommissionierteMenge < zu kommissionierteMenge
-
+                        if(kommissionierteMenge < 1){
+                            throw new Exception();
+                        }
                         artikelZaehler++;
+                        setTableRows();
+                        //@TODO Prüfen ob kommissionierteMenge < zu kommissionierteMenge
                     }
                     catch (Exception e){
-                        Context context = getApplicationContext();
-                        CharSequence text = "Eingegebene Menge ist nicht gültig";
-                        int duration = Toast.LENGTH_SHORT;
-                        Toast toast = Toast.makeText(context, text, duration);
-                        toast.show();
+                        // Keine Eingabe oder zu große Eingabe
+                        showToast("Eingegebene Menge ist nicht gültig");
+
                     }
-
-
+                    // Wenn Letzter Artikel Button beschriftung ändern
                     if(artikelZaehler >= artikelGesamt){
                         weiter_btn.setText("Kommession abschließen");
                     }
@@ -91,22 +72,38 @@ public class CommissionArtikel extends AppCompatActivity {
         });
 
     }
+    private void setTableRows(){
+        // Setzen der neuen Überschrift
+        TextView artikelanzahlLabel = (TextView) findViewById(R.id.commission_artikelAnzahl_label);
+        artikelanzahlLabel.setText("Artikel " + artikelZaehler + " von " + artikelGesamt);
+        // Setzen der ganzen Zeilen
+        TextView artikelCode = (TextView) findViewById(R.id.commission_artikel_artikel_code);
+        artikelCode.setText("456");
 
-    private void printTable(){
-        TableLayout table = (TableLayout) findViewById(R.id.table_layout);
-        TableRow row = new TableRow(this);
+        TextView artikelName = (TextView) findViewById(R.id.commission_artikel_artikel_name);
+        artikelName.setText("asdfsdklasd");
 
+        TextView lagerort = (TextView) findViewById(R.id.commission_artikel_artikel_storage);
+        lagerort.setText("5");
 
-        // Erzeugen der Spalten
+        TextView lagerbestand = (TextView) findViewById(R.id.commission_artikel_artikel_soll);
+        lagerbestand.setText("30");
 
-
-            //Hinzufügen der Spalten
-
-
-
-            table.addView(row);
-
+        TextView kommissionsMenge = (TextView) findViewById(R.id.commission_artikel_artikel_commession);
+        kommissionsMenge.setText("5");
     }
 
+    private void showToast(String text){
+        Context context = getApplicationContext();
 
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    // Verhindern das durch zurück Button kommission abgebrochen wird
+    @Override
+    public void onBackPressed() {
+        showToast("Kommission kann nicht abgebrochen werden");
+    }
 }
