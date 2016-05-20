752c092dc6c2cb67c3c0532c7495a721b3fcc472
hammers182 <marco_hoelscher@gmx.net>
2016-05-20 13:06:54 +0200

    Add Stock Activity

---
+++ app/src/main/java/warehouse/fh_muenster/de/warehouse/Stock.java
@@ -0,0 +1,13 @@
+package warehouse.fh_muenster.de.warehouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Stock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock);
+    }
+}
