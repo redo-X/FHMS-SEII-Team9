715097ce4560eaa31c767fcb6f3ae1dda3274ea7
hammers182 <marco_hoelscher@gmx.net>
2016-05-30 11:10:24 +0200

    Change StockAmendment

--- app/src/main/java/warehouse/fh_muenster/de/warehouse/StockAmendment.java
+++ app/src/main/java/warehouse/fh_muenster/de/warehouse/StockAmendment.java
@@ -2,12 +2,45 @@ package warehouse.fh_muenster.de.warehouse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class StockAmendment extends AppCompatActivity {
+
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_amendment);
+
+        //Erstelle DrawerMenu
+        mDrawerList = (ListView) findViewById(R.id.navList);
+        addDrawerItems();
+    }
+
+
+    /**
+     * Hilfsmethode für DrawerMenu
+     */
+    private void addDrawerItems() {
+        String[] MenuArray = {"Meine Kommissionen", "Offene Kommissionen", "LogOut"};
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, MenuArray);
+        mDrawerList = (ListView) findViewById(R.id.navList);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String MenuArray = String.valueOf(parent.getItemAtPosition(position));
+                        Toast.makeText(StockAmendment.this, MenuArray, Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
 }
