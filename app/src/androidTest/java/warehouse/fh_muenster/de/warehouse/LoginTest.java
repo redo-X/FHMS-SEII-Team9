2496e36ae8cb8e30cd591b32a81d5545a6d66646
FutureDragon <future_dragon@hotmail.de>
2016-06-12 15:03:42 +0200

    Add Espresso Tests

--- app/src/androidTest/java/warehouse/fh_muenster/de/warehouse/LoginTest.java
+++ app/src/androidTest/java/warehouse/fh_muenster/de/warehouse/LoginTest.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.test.ApplicationTestCase;
+
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intended;
 
 
@@ -16,12 +18,13 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest  {
+public class LoginTest {
     @Rule
     public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<>(
             LoginActivity.class);
@@ -30,7 +33,7 @@ public class ApplicationTest  {
      * Checks if the Login is successful
      */
     @Test
-    public void loginTest(){
+    public void testLogin(){
         try {
             Intents.init();
             onView(withId(R.id.mitarbeiterNr_txt))
@@ -48,6 +51,37 @@ public class ApplicationTest  {
         }
     }
 
+    /**
+     * Checks if the Login Fail with wrong user data
+     */
+    @Test
+    public void testLoginFail(){
+        onView(withId(R.id.mitarbeiterNr_txt))
+                .perform(typeText("456"), closeSoftKeyboard());
+        onView(withId(R.id.password_txt))
+                .perform(typeText("auhkfsduhjklf"), closeSoftKeyboard());
+        onView(withId(R.id.login_btn)).perform(click());
+        onView(withId(R.id.login_btn)).check(matches(isDisplayed()));
+    }
+
+
+    /**
+     * Checks if the Scanner start by clicking the Scanner Button
+     */
+    @Test
+    public void testScanner(){
+        try {
+            Intents.init();
+            onView(withId(R.id.scanner_scan_btn)).perform(click());
+            intended(hasComponent(Scanner.class.getName()));
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            Intents.release();
+        }
+    }
 
 
 }
