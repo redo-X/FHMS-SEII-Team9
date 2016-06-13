4f245949fa0848d49c4a9addd49cd228332391e7
hammers182 <marco_hoelscher@gmx.net>
2016-06-13 12:07:03 +0200

    Update Menu Stock

--- app/src/main/java/warehouse/fh_muenster/de/warehouse/Stock.java
+++ app/src/main/java/warehouse/fh_muenster/de/warehouse/Stock.java
@@ -2,10 +2,19 @@ package warehouse.fh_muenster.de.warehouse;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -18,6 +27,12 @@ import warehouse.fh_muenster.de.warehouse.Server.ServerMockImple;
 
 public class Stock extends AppCompatActivity {
 
+    private ListView mListLayout;
+    private ArrayAdapter<String> mAdapter;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mActivityTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,13 +41,22 @@ public class Stock extends AppCompatActivity {
         printTable();
 
         Button stockA = (Button) findViewById(R.id.testButtonStockA);
-        stockA.setOnClickListener(new View.OnClickListener(){
+        stockA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), StockAmendment.class);
                 startActivity(i);
             }
         });
+
+        //Drawer Menu
+        mListLayout = (ListView) findViewById(R.id.navList);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+        addDrawerItems();
+        setupDrawer();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
     private void printTable() {
@@ -89,7 +113,131 @@ public class Stock extends AppCompatActivity {
 
         aendernbutton.setId(i);
 
-
         return aendernbutton;
+    }
+
+    //Drawer Menu
+    private void addDrawerItems() {
+        final WarehouseApplication myApp = (WarehouseApplication) getApplication();
+        final Employee employee = myApp.getEmployee();
+
+        if (employee.getRole().equals(Role.Kommissionierer)) {
+            String[] menuArray = {"Meine Kommissionen", "Offene Kommissionen", "Logout"};
+            mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menuArray) {
+                @Override
+                public View getView(int position, View convertView, ViewGroup parent) {
+                    View view = super.getView(position, convertView, parent);
+                    TextView text = (TextView) view.findViewById(android.R.id.text1);
+                    text.setTextColor(Color.BLUE);
+                    if (text.getText().toString().equals("Lagerbestände")) {
+                        text.setTextColor(Color.parseColor("#BDBDBD"));
+                    }
+                    return view;
+                }
+            };
+        } else {
+            String[] menuArray = {"Meine Kommissionen", "Offene Kommissionen", "Lagerbestände", "Logout"};
+            mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menuArray) {
+                @Override
+                public View getView(int position, View convertView, ViewGroup parent) {
+                    View view = super.getView(position, convertView, parent);
+                    TextView text = (TextView) view.findViewById(android.R.id.text1);
+                    text.setTextColor(Color.BLUE);
+
+                    if (text.getText().toString().equals("Lagerbestände")) {
+                        text.setTextColor(Color.parseColor("#BDBDBD"));
+                    }
+                    return view;
+                }
+            };
+        }
+
+        mListLayout = (ListView) findViewById(R.id.navList);
+        mListLayout.setAdapter(mAdapter);
+
+        mListLayout.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        switch (position) {
+                            case 0:
+                                Intent newActivity0 = new Intent(getApplicationContext(), CommissioningOverview.class);
+                                newActivity0.putExtra("screen", "myCommission");
+                                newActivity0.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                Stock.this.startActivity(newActivity0);
+                                break;
+                            case 1:
+                                Intent newActivity1 = new Intent(getApplicationContext(), CommissioningOverview.class);
+                                newActivity1.putExtra("screen", "commissionOverview");
+                                newActivity1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                Stock.this.startActivity(newActivity1);
+                                break;
+                            case 2:
+                                if (employee.getRole().equals(Role.Kommissionierer)) {
+                                    Intent newActivity2 = new Intent(getApplicationContext(), LoginActivity.class);
+                                    newActivity2.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    Stock.this.startActivity(newActivity2);
+                                    break;
+                                } else {
+                                    Intent newActivity2 = new Intent(getApplicationContext(), Stock.class);
+                                    newActivity2.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    Stock.this.startActivity(newActivity2);
+                                    break;
+                                }
+                            case 3:
+                                myApp.setOpenCommissionsMap(null);
+                                myApp.setPickerCommissionsMap(null);
+                                myApp.setEmployee(null);
+
+                                Intent newActivity3 = new Intent(getApplicationContext(), LoginActivity.class);
+                                newActivity3.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                Stock.this.startActivity(newActivity3);
+                                break;
+                        }
+                        finish();
+                    }
+                }
+        );
+    }
+
+    //Drawer Menu
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(R.string.drawer_title);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu();
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    //Drawer Menu
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    //Drawer Menu
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    //Drawer Menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return mDrawerToggle.onOptionsItemSelected(item);
     }
 }
